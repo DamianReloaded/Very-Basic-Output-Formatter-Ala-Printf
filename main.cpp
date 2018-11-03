3bab54bc045e3de1ad6763da53339c342c825f2f
damian <damian@owl>
2018-11-02 21:57:19 -0300

    profiling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,70 @@
+namespace impl {
+  template<char... S> struct str {
+    static constexpr char data[] = { S..., '\0' };
+  };
+
+  template<char... S>
+  constexpr char str<S...>::data[];
+
+  template<char C, int N, char const * const * Ptr, char... S>
+  struct explode_ { typedef typename explode_<*(*Ptr+N+1), N+1, Ptr, S..., C>::list list; };
+
+  template<int N, char const * const * Ptr, char... S>
+  struct explode_<'\0', N, Ptr, S...> { typedef str<S...> list; };
+
+  template<char const * const * Ptr>
+  using explode = typename explode_<**Ptr, 0, Ptr>::list;
+
+  template<class...>
+  struct join { typedef str<> result; };
+
+  template<char... A, char... B, class... Ts>
+  struct join<str<A...>, str<B...>, Ts...> {
+    typedef typename join<str<A..., ',', ' ', B...>, Ts...>::result result;
+  };
+
+  template<char... A>
+  struct join<str<A...>> {
+    typedef str<A..., '\n'> result;
+  };
+}
+
+#include <cstddef>
+
+
+template<class T> struct format_m;
+template<class T> struct format_m<T*>      { static constexpr char const * fmt = "%p";  };
+template<> struct format_m<int          >  { static constexpr char const * fmt = "%d";  };
+template<> struct format_m<float        >  { static constexpr char const * fmt = "%.4f";  };
+template<> struct format_m<double        >  { static constexpr char const * fmt = "%.4f";  };
+template<> struct format_m<char const*  >  { static constexpr char const * fmt = "%s";  };
+template<> struct format_m<char         >  { static constexpr char const * fmt = "'%c'";  };
+template<> struct format_m<unsigned long>  { static constexpr char const * fmt = "%lu"; };
+
+
+template<class... Ts>
+constexpr char const * build_fmt () {
+  return impl::join<impl::explode<&format_m<Ts>::fmt>...>::result::data;
+}
+
+/* ...................................... */
+
+#include <cstdio>
+#include <type_traits>
+
+template<class... Ts>
+void auto_printf (Ts... args) {
+  constexpr char const * fmt = build_fmt<Ts...> ();
+  printf (fmt, args...);
+}
+
+//--------------------------------------------------------
 #include <vector>
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+
+
 
 namespace owl
 {
@@ -11,7 +74,7 @@ namespace owl
         switch (type)
         {
             case 's': ss << value; break;
-            case 'd': ss << std::fixed << std::setprecision(1) << value; break;
+            case 'd': ss << std::fixed << std::setprecision(4) << value; break;
         }
     }
 
@@ -57,9 +120,31 @@ namespace owl
         return ss.str();
     }
 }
+
+#include <time.h>
+
 int main()
 {
-    owl::out("{s} world{s} {d}\n","Hello",'!',123.2312);
-    std::cout << owl::fmt("{s} world{s} {d}\n","Hello",'!',123.2312);
+    clock_t t;
+    clock_t t1, t2, t3;
+
+    t = clock();
+    for (size_t i=0; i<10000; i++) auto_printf ("Hello " "world" "!",123.2312);
+    t1 = clock() - t;
+
+    t = clock();
+    for (size_t i=0; i<10000; i++) owl::out("{s} world{s} {d}\n","Hello",'!',123.2312);
+    t2 = clock() - t;
+
+    t = clock();
+    for (size_t i=0; i<10000; i++) printf("Hello ! %f \n",123.2312);
+    t3 = clock() - t;
+
+    printf ("auto_printf: took me %d clicks (%f seconds).\n",t1,((float)t1)/CLOCKS_PER_SEC);
+    printf ("owl::out   : took me %d clicks (%f seconds).\n",t2,((float)t2)/CLOCKS_PER_SEC);
+    printf ("printf     : took me %d clicks (%f seconds).\n",t3,((float)t3)/CLOCKS_PER_SEC);
+
+
     return 0;
 }
+
